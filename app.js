dc1295506865958c62d425b6f16df73c4d358010
HolyWaffels <41336951+HolyWaffels@users.noreply.github.com>
2019-09-16 21:47:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//opdracht 1
+const remainder = 17 % 2;
+
+console.log(remainder)
+
+//opdracht 2
+const message = 'Programming is not so cool';
+
+console.log(message)
+console.log(message.replace('not', ''));
+
+//opdracht 3
+//Ik snap niet goed wat hier de bedoeling is, deze oplossing heb ik op het internet gevonden. maar ik denk niet dat het de goede oplossing is.
+var str1 = '1400';
+var str2 = 'Ik woon in Naboo';
+var n = str1.localeCompare(str2);
+
+console.log(str1.localeCompare(str2));
