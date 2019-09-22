3c55e9ad0152e3b1e5ff117dec6f5912abd6245d
HolyWaffels <41336951+HolyWaffels@users.noreply.github.com>
2019-09-22 13:31:39 +0200

    Add files via upload

---
+++ les 4/assets/app.js
@@ -0,0 +1,96 @@
+//Opdracht 1
+//If Else
+{
+  let cijfer = 5;
+
+  if (cijfer <6)
+    console.log('onvoldoende')
+
+  else if (cijfer <=7)
+    console.log('voldoende')
+
+  else if (cijfer <=9)
+    console.log('goed')
+
+  else if (cijfer >=10)
+    console.log('uitmuntend')
+}
+
+//Opdracht 2
+//Switch
+  {
+    let cijfer = 6;
+
+    switch (cijfer) {
+      case (1):
+      console.log('onvoldoende')
+
+        break;
+
+      case (2):
+      console.log('onvoldoende')
+
+        break;
+
+      case (3):
+      console.log('onvoldoende')
+
+        break;
+
+      case (4):
+      console.log('onvoldoende')
+
+        break;
+
+      case (5):
+      console.log('onvoldoende')
+
+        break;
+
+      case (6):
+      console.log('voldoende')
+
+        break;
+
+      case (7):
+      console.log('voldoende')
+
+        break;
+
+      case (8):
+      console.log('goed')
+
+        break;
+
+      case (9):
+      console.log('uitmuntend')
+
+        break;
+
+      case (10):
+      console.log('uitmuntend')
+
+        break;
+
+
+      default:
+          console.log('cijfer is ongeldig')
+        break;
+    }
+
+    }
+
+
+//oefening 3
+
+  {
+    let purchasedBook = true;
+    let job = 'teacher';
+    let inTrain = true;
+
+    if (purchasedBook = true && inTrain = true && job = 'teacher') {
+        console.log('finally i can enjoy my book!')
+
+    else (console.log('this is false'))
+    }
+  }
