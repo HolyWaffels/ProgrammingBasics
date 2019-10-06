1a8dca01277a8a5a839a762766b055f9ab3ae0c8
HolyWaffels <41336951+HolyWaffels@users.noreply.github.com>
2019-10-06 16:10:30 +0200

    Add files via upload

---
+++ les 8/assets/app.js
@@ -0,0 +1,52 @@
+//huiswerk opdracht 1
+{
+  const lapRound = [
+    {
+      tijd: 55.99
+    },
+    {
+      tijd: 63.00
+    },
+    {
+      tijd: 63.01
+    },
+    {
+      tijd: 54.01
+    },
+    {
+      tijd: 62.79
+    },
+    {
+      tijd: 52.88
+    },
+    {
+      tijd: 53.10
+    },
+    {
+      tijd: 54.12
+    }
+  ];
+}
+
+//huiswerk opdracht 2
+{
+  const teachers = [
+    {
+        name: "Loek",
+        profession: "Teacher",
+        brand: "Linux"
+    },
+    {
+        name: "Daan",
+        profession: "Teacher",
+        brand: "Arduino"
+    },
+    {
+        name: "Rimmert",
+        profession: "Teacher",
+        brand: "Apple"
+    }
+]
+
+      console.log(`I have a ${teachers.profession} named ${teachers.name} and he likes to work on a ${teachers.brand} computer`)
+}
